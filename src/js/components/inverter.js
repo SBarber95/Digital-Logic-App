90882f005f3b61c31cabc388c7df7667e145980c
SBarber95 <savannahlee117@gmail.com>
2016-12-12 15:55:43 -0500

    Continue encapsulation of SVG into Vue components

---
+++ src/js/components/inverter.js
@@ -0,0 +1,14 @@
+/**
+ * Created by savannah on 12/12/2016.
+ */
+var inverter = new Vue({
+    el: '.insertion-point',
+    template: '<svg xmlns="http://www.w3.org/2000/svg" width="109.95652" height="54.8125" class="inverter" version="1.0">'+
+    '<g transform="translate(-5.0434781,-2.59375)">'+
+    '<path style="stroke-width:2.4454813" d="M 95.634522,30 115,30" class="inverter-output"/>'+
+    '<path style="stroke-width:2.44548154" d="M 34.379389,30 5.0434781,30" class="inverter-input"/>'+
+    '<path  d="m 34.288047,2.59375 0,2.9432096 0,48.9260804 0,2.94321 2.635648,-1.299599 50.153713,-24.46304 0,-3.287222 L 36.923695,3.893349 34.288047,2.59375 Z M 37.955036,8.4801691 82.111693,30 37.955036,51.519831 l 0,-43.0396619 z" class="inverter-component"/>'+
+    '<ellipse class="gate-inverter" cx="90.553406" cy="30" rx="4.8893185" ry="4.8926082"/>'+
+    '</g>'+
+    '</svg>'
+});
