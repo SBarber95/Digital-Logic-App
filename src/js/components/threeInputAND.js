4b03f924db417c97e85bdbcb7367521f6e6558f3
SBarber95 <savannahlee117@gmail.com>
2016-12-13 20:27:27 -0500

    Continue set up of circuit components (added function to add some components to page; drag)

--- src/js/components/threeInputAND.js
+++ src/js/components/threeInputAND.js
@@ -1,15 +1,37 @@
 /**
  * Created by savannah on 12/12/2016.
  */
-var threeInputAND = new Vue({
-    el: '.insertion-point',
-    template: '<svg xmlns="http://www.w3.org/2000/svg" width="105.10703" height="54.930817" class="three-input-AND" version="1">'+
-    '<g transform="matrix(1.234375,0,0,1.3732704,-12.158594,-6.866352)">'+
-    '<path d="m 74.98,25.13 c 16.01,0 20.02,0 20.02,0" class="gate-output" style="stroke-width:1.78999996"></path>'+
-    '<path d="M 31,12 10.11,12" class="gate-input" style="stroke-width:1.78999996"></path>'+
-    '<path d="M 32,38 9.98,38" class="gate-input" style="stroke-width:1.80999994"></path>'+
-    '<path d="m 30,5 0,1.43 0,37.14 0,1.43 1.59,0 21.15,0 c 12.51,0 22.2,-9 22.2,-20 0,-11 -9.69,-20 -22.2,-20 0,0 0,0 -21.15,0 L 30,5 Z m 3.17,2.86 c 8.86,0 14.5,0 16.92,0 1.32,0 1.98,0 2.31,0 0.17,0 0.26,0 0.3,0 0.02,0 0.03,0 0.03,0 10.84,0 18.5,7.64 18.5,17.14 0,9.5 -8.2,17.14 -19.03,17.14 l -19.03,0 0,-34.29 z" class="three-AND"></path>'+
-    '<path d="M 31,25 9.85,25" class="gate-input" style="stroke-width:1.79999995"></path>'+
-    '</g>'+
-    '</svg>'
+
+$("#select3InputAND").click(function() {
+
+    var threeInputAND = new Vue({
+        el: '.insertion-point',
+        template: '<svg xmlns="http://www.w3.org/2000/svg" width="105.10703" height="54.930817" class="component three-input-AND" version="1">'+
+        '<g transform="matrix(1.234375,0,0,1.3732704,-12.158594,-6.866352)">'+
+        '<path d="m 74.98,25.13 c 16.01,0 20.02,0 20.02,0" class="gate-output" style="stroke-width:1.78999996"></path>'+
+        '<path d="M 31,12 10.11,12" class="gate-input" style="stroke-width:1.78999996"></path>'+
+        '<path d="M 32,38 9.98,38" class="gate-input" style="stroke-width:1.80999994"></path>'+
+        '<path d="m 30,5 0,1.43 0,37.14 0,1.43 1.59,0 21.15,0 c 12.51,0 22.2,-9 22.2,-20 0,-11 -9.69,-20 -22.2,-20 0,0 0,0 -21.15,0 L 30,5 Z m 3.17,2.86 c 8.86,0 14.5,0 16.92,0 1.32,0 1.98,0 2.31,0 0.17,0 0.26,0 0.3,0 0.02,0 0.03,0 0.03,0 10.84,0 18.5,7.64 18.5,17.14 0,9.5 -8.2,17.14 -19.03,17.14 l -19.03,0 0,-34.29 z" class="three-AND"></path>'+
+        '<path d="M 31,25 9.85,25" class="gate-input" style="stroke-width:1.79999995"></path>'+
+        '</g>'+
+        '</svg>'
+    });
+
+    // Recreates insertion point to allow the addition of multiple components
+    var canvas = document.getElementById("canvas");
+    canvas.innerHTML += '<div class="insertion-point"></div>';
+
+    // Makes sure all instances of this component remain draggable
+    var draggableElems = document.querySelectorAll('.component');
+
+    var draggies = [];
+
+    for ( var i=0; i < draggableElems.length; i++ ) {
+        var draggableElem = draggableElems[i];
+        var draggie = new Draggabilly( draggableElem, {
+            containment: true
+        });
+        draggies.push( draggie );
+    }
+
 });
--- src/js/components/threeInputNAND.js
+++ src/js/components/threeInputNAND.js
@@ -1,16 +1,37 @@
 /**
  * Created by savannah on 12/12/2016.
  */
-var threeInputNAND = new Vue({
-    el: '.insertion-point',
-    template: '<svg xmlns="http://www.w3.org/2000/svg" width="110.25" height="55" class="three-input-NAND" version="1.0">'+
-    '<g transform="translate(-5,-5)">'+
-    '<path style="stroke-width:2.59566951" d="m 95.65,32.5 c 15.68,0 19.6,0 19.6,0" class="gate-output"/>'+
-    '<path style="stroke-width:2.59566951" d="m 36.85,14.860913 -31.85,0" class="gate-input"/>'+
-    '<path style="stroke-width:2.59566927" d="m 38.075,49.962311 -33.075,0" class="gate-input"/>'+
-    '<path d="m 35.625,5 0,1.9642857 0,51.0714293 0,1.964285 1.75,0 23.333333,0 C 74.51152,60 85.208332,47.624938 85.208332,32.5 85.208332,17.375062 74.511521,5.0000002 60.708333,5 c 0,0 0,0 -23.333333,0 l -1.75,0 z m 3.5,3.9285715 c 9.771974,0 16.000001,0 18.666667,0 1.458333,0 2.187499,0 2.552082,0 0.182292,0 0.282554,0 0.328126,0 0.02278,0 0.03076,0 0.03645,0 11.956813,10e-8 20.416668,10.5049385 20.416668,23.5714285 0,13.06649 -9.043189,23.571428 -21,23.571428 l -21,0 0,-47.1428565 z" class="three-NAND"/>'+
-    '<ellipse class="gate-inverter" cx="90.75" cy="32.5" rx="4.9000001" ry="5.5" />'+
-    '<path style="stroke-width:2.59566951" d="m 36.799748,32.486544 -31.8500005,0" class="gate-input"/>'+
-    '</g>'+
-    '</svg>'
+
+$("#select3InputNAND").click(function() {
+    var threeInputNAND = new Vue({
+        el: '.insertion-point',
+        template: '<svg xmlns="http://www.w3.org/2000/svg" width="110.25" height="55" class="component three-input-NAND" version="1.0">'+
+        '<g transform="translate(-5,-5)">'+
+        '<path style="stroke-width:2.59566951" d="m 95.65,32.5 c 15.68,0 19.6,0 19.6,0" class="gate-output"/>'+
+        '<path style="stroke-width:2.59566951" d="m 36.85,14.860913 -31.85,0" class="gate-input"/>'+
+        '<path style="stroke-width:2.59566927" d="m 38.075,49.962311 -33.075,0" class="gate-input"/>'+
+        '<path d="m 35.625,5 0,1.9642857 0,51.0714293 0,1.964285 1.75,0 23.333333,0 C 74.51152,60 85.208332,47.624938 85.208332,32.5 85.208332,17.375062 74.511521,5.0000002 60.708333,5 c 0,0 0,0 -23.333333,0 l -1.75,0 z m 3.5,3.9285715 c 9.771974,0 16.000001,0 18.666667,0 1.458333,0 2.187499,0 2.552082,0 0.182292,0 0.282554,0 0.328126,0 0.02278,0 0.03076,0 0.03645,0 11.956813,10e-8 20.416668,10.5049385 20.416668,23.5714285 0,13.06649 -9.043189,23.571428 -21,23.571428 l -21,0 0,-47.1428565 z" class="three-NAND"/>'+
+        '<ellipse class="gate-inverter" cx="90.75" cy="32.5" rx="4.9000001" ry="5.5" />'+
+        '<path style="stroke-width:2.59566951" d="m 36.799748,32.486544 -31.8500005,0" class="gate-input"/>'+
+        '</g>'+
+        '</svg>'
+    });
+
+    // Recreates insertion point to allow the addition of multiple components
+    var canvas = document.getElementById("canvas");
+    canvas.innerHTML += '<div class="insertion-point"></div>';
+
+    // Makes sure all instances of this component remain draggable
+    var draggableElems = document.querySelectorAll('.component');
+
+    var draggies = [];
+
+    for ( var i=0; i < draggableElems.length; i++ ) {
+        var draggableElem = draggableElems[i];
+        var draggie = new Draggabilly( draggableElem, {
+            containment: true
+        });
+        draggies.push( draggie );
+    }
+
 });
