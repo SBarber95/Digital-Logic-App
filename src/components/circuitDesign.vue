acb6449d6065ca1b5e4724c9e2b695d56aa4a068
Savannah Barber <savannahlee117@gmail.com>
2017-08-03 14:13:23 -0400

    Temp changes for in-progress circuit fix

--- src/components/circuitDesign.vue
+++ src/components/circuitDesign.vue
@@ -133,12 +133,10 @@
                         </div>
                     </div>
                 </div>
-                <button id="delete_circuit" style="position: relative; left: 74%; top: 70px;"
-                        class="btn btn-danger">Delete Circuit
-                </button>
-                <button id="simulate" @click="simulate"
-                        style="position: relative; left: 63%; top: 130px;" class="btn btn-primary">Simulate
-                </button>
+                <input type="button" id="delete_circuit" style="position: relative; left: 74%; top: 70px;"
+                        class="btn btn-danger" value="Delete Circuit">
+                <input type="submit" id="simulate" @click="simulate"
+                        style="position: relative; left: 63%; top: 130px;" class="btn btn-primary" value="Simulate">
             </div>
         </div>
         <nav id="context-menu" class="context-menu">
@@ -160,7 +158,150 @@
     import Snap from 'snapsvg'
     import TreeModel from 'tree-model'
     import $ from 'jquery'
-    import circuitDesignMixin from "../mixins/circuitDesignMixin";
+
+    function ThreeInputAND (id, input1, input2, input3, inputConnections, outputConnections) {
+
+        this.id = id;
+        this.input1 = input1;
+        this.input2 = input2;
+        this.input3 = input3;
+        this.inputConnections = inputConnections;
+        this.outputConnections = outputConnections;
+
+        this.output = function() {
+            return (this.input1 && this.input2 && this.input3);
+        }
+
+    }
+
+    function ThreeInputNAND (id, input1, input2, input3, inputConnections, outputConnections) {
+
+        this.id = id;
+        this.input1 = input1;
+        this.input2 = input2;
+        this.input3 = input3;
+        this.inputConnections = inputConnections;
+        this.outputConnections = outputConnections;
+
+        this.output = function() {
+            return !(this.input1 && this.input2 && this.input3);
+        }
+
+    }
+
+    function ThreeInputNOR (id, input1, input2, input3, inputConnections, outputConnections) {
+
+        this.id = id;
+        this.input1 = input1;
+        this.input2 = input2;
+        this.input3 = input3;
+        this.inputConnections = inputConnections;
+        this.outputConnections = outputConnections;
+
+        this.output = function() {
+            return !(this.input1 || this.input2 || this.input3);
+        }
+
+    }
+
+    function ThreeInputOR (id, input1, input2, input3, inputConnections, outputConnections) {
+
+        this.id = id;
+        this.input1 = input1;
+        this.input2 = input2;
+        this.input3 = input3;
+        this.inputConnections = inputConnections;
+        this.outputConnections = outputConnections;
+
+        this.output = function() {
+            return (this.input1 || this.input2 || this.input3);
+        }
+
+    }
+
+    function TwoInputAND (id, input1, input2, inputConnections, outputConnections) {
+
+        this.id = id;
+        this.input1 = input1;
+        this.input2 = input2;
+        this.inputConnections = inputConnections;
+        this.outputConnections = outputConnections;
+
+        this.output = function() {
+            return (this.input1 && this.input2);
+        }
+
+    }
+
+    function TwoInputNAND (id, input1, input2, inputConnections, outputConnections) {
+
+        this.id = id;
+        this.input1 = input1;
+        this.input2 = input2;
+        this.inputConnections = inputConnections;
+        this.outputConnections = outputConnections;
+
+        this.output = function() {
+            return !(this.input1 && this.input2);
+        }
+
+    }
+
+    function TwoInputNOR (id, input1, input2, inputConnections, outputConnections) {
+
+        this.id = id;
+        this.input1 = input1;
+        this.input2 = input2;
+        this.inputConnections = inputConnections;
+        this.outputConnections = outputConnections;
+
+        this.output = function() {
+            return !(this.input1 || this.input2);
+        }
+
+    }
+
+    function TwoInputOR (id, input1, input2, inputConnections, outputConnections) {
+
+        this.id = id;
+        this.input1 = input1;
+        this.input2 = input2;
+        this.inputConnections = inputConnections;
+        this.outputConnections = outputConnections;
+
+        this.output = function() {
+            return (this.input1 || this.input2);
+        }
+
+    }
+
+    function TwoInputXNOR (id, input1, input2, inputConnections, outputConnections) {
+
+        this.id = id;
+        this.input1 = input1;
+        this.input2 = input2;
+        this.inputConnections = inputConnections;
+        this.outputConnections = outputConnections;
+
+        this.output = function() {
+            return !(this.input1 ^ this.input2);
+        }
+
+    }
+
+    function TwoInputXOR (id, input1, input2, inputConnections, outputConnections) {
+
+        this.id = id;
+        this.input1 = input1;
+        this.input2 = input2;
+        this.inputConnections = inputConnections;
+        this.outputConnections = outputConnections;
+
+        this.output = function() {
+            return (this.input1 ^ this.input2);
+        }
+
+    }
 
     // Wire class definition
     function Wire(outputSide, inputSide, inputNumber, wireId, outputX, outputY, inputX, inputY, outputCompOffsetX, outputCompOffsetY,
@@ -182,7 +323,6 @@
     }
 
     export default {
-        mixins: [circuitDesignMixin],
         data() {
             return {
                 // TODO: PERHAPS STORE INFO OF LATEST COMPONENT OR WIRE MADE FOR UNDO PURPOSES
@@ -201,14 +341,227 @@
             }
         },
         mounted () {
+            let snap = Snap('#canvas');
             let vm = this;
+
+            let newComponent;
+            let canvas = document.getElementById("canvas");
+
+            $('#select3InputNAND').click(function() {
+
+                Snap.load("src/components/circuitComponents/threeInputNAND.vue", function(e) {
+                    newComponent = e.select("g");
+                    snap.append(newComponent);
+                    newComponent.drag();
+
+                    // Sets a unique id to the rendered component
+                    var newSVG = canvas.lastElementChild;
+                    var uniqueID = "component-" + vm.idNum;
+                    newSVG.setAttribute("id", uniqueID);
+
+                    // Create default 3 input NAND gate
+                    vm.circuitComponents[vm.idNum] = new ThreeInputNAND(uniqueID, false, false, false, [], []);
+                    vm.idNum++;
+
+                });
+
+            });
+
+            $('#select3InputAND').click(function() {
+
+                Snap.load("src/components/circuitComponents/threeInputAND.vue", function(e) {
+                    newComponent = e.select("g");
+                    snap.append(newComponent);
+                    newComponent.drag();
+
+                    // Sets a unique id to the rendered component
+                    var newSVG = canvas.lastElementChild;
+                    var uniqueID = "component-" + vm.idNum;
+                    newSVG.setAttribute("id", uniqueID);
+
+                    // Create default 3 input AND gate
+                    vm.circuitComponents[vm.idNum] = new ThreeInputAND(uniqueID, false, false, false, [], []);
+                    vm.idNum++;
+
+                });
+
+            });
+
+            $("#select3InputNOR").click(function() {
+
+                Snap.load("src/components/circuitComponents/threeInputNOR.vue", function(e) {
+                    newComponent = e.select("g");
+                    snap.append(newComponent);
+                    newComponent.drag();
+
+                    // Sets a unique id to the rendered component
+                    var newSVG = canvas.lastElementChild;
+                    var uniqueID = "component-" + vm.idNum;
+                    newSVG.setAttribute("id", uniqueID);
+
+                    // Create default 3 input NOR gate
+                    vm.circuitComponents[vm.idNum] = new ThreeInputNOR(uniqueID, false, false, false, [], []);
+                    vm.idNum++;
+
+                });
+
+            });
+
+            $("#select3InputOR").click(function() {
+
+                Snap.load("src/components/circuitComponents/threeInputOR.vue", function(e) {
+                    newComponent = e.select("g");
+                    snap.append(newComponent);
+                    newComponent.drag();
+
+                    // Sets a unique id to the rendered component
+                    var newSVG = canvas.lastElementChild;
+                    var uniqueID = "component-" + vm.idNum;
+                    newSVG.setAttribute("id", uniqueID);
+
+                    // Create default 3 input OR gate
+                    vm.circuitComponents[vm.idNum] = new ThreeInputOR(uniqueID, false, false, false, [], []);
+                    vm.idNum++;
+
+                });
+
+            });
+
+            $("#select2InputXOR").click(function() {
+
+                Snap.load("src/components/circuitComponents/twoInputXOR.vue", function(e) {
+                    newComponent = e.select("g");
+                    snap.append(newComponent);
+                    newComponent.drag();
+
+                    // Sets a unique id to the rendered component
+                    var newSVG = canvas.lastElementChild;
+                    var uniqueID = "component-" + vm.idNum;
+                    newSVG.setAttribute("id", uniqueID);
+
+                    // Create default 2 input XOR gate
+                    vm.circuitComponents[vm.idNum] = new TwoInputXOR(uniqueID, false, false, [], []);
+                    vm.idNum++;
+
+                });
+
+            });
+
+            $("#select2InputXNOR").click(function() {
+
+                Snap.load("src/components/circuitComponents/twoInputXNOR.vue", function(e) {
+                    newComponent = e.select("g");
+                    snap.append(newComponent);
+                    newComponent.drag();
+
+                    // Sets a unique id to the rendered component
+                    var newSVG = canvas.lastElementChild;
+                    var uniqueID = "component-" + vm.idNum;
+                    newSVG.setAttribute("id", uniqueID);
+
+                    // Create default 2 input XNOR gate
+                    vm.circuitComponents[vm.idNum] = new TwoInputXNOR(uniqueID, false, false, [], []);
+                    vm.idNum++;
+
+                });
+
+            });
+
+            $("#select2InputOR").click(function() {
+
+                Snap.load("src/components/circuitComponents/twoInputOR.vue", function(e) {
+                    newComponent = e.select("g");
+                    snap.append(newComponent);
+                    newComponent.drag();
+
+                    // Sets a unique id to the rendered component
+                    var newSVG = canvas.lastElementChild;
+                    var uniqueID = "component-" + vm.idNum;
+                    newSVG.setAttribute("id", uniqueID);
+
+                    // Create default 2 input OR gate
+                    vm.circuitComponents[vm.idNum] = new TwoInputOR(uniqueID, false, false, [], []);
+                    vm.idNum++;
+
+                });
+
+            });
+
+            $("#select2InputNOR").click(function() {
+
+                Snap.load("src/components/circuitComponents/twoInputNOR.vue", function(e) {
+                    newComponent = e.select("g");
+                    snap.append(newComponent);
+                    newComponent.drag();
+
+                    // Sets a unique id to the rendered component
+                    var newSVG = canvas.lastElementChild;
+                    var uniqueID = "component-" + vm.idNum;
+                    newSVG.setAttribute("id", uniqueID);
+
+                    // Create default 2 input NOR gate
+                    vm.circuitComponents[vm.idNum] = new TwoInputNOR(uniqueID, false, false, [], []);
+                    vm.idNum++;
+
+                });
+
+            });
+
+            $("#select2InputNAND").click(function() {
+
+                Snap.load("src/components/circuitComponents/twoInputNAND.vue", function(e) {
+                    newComponent = e.select("g");
+                    snap.append(newComponent);
+                    newComponent.drag();
+
+                    // Sets a unique id to the rendered component
+                    var newSVG = canvas.lastElementChild;
+                    var uniqueID = "component-" + vm.idNum;
+                    newSVG.setAttribute("id", uniqueID);
+
+                    // Create default 2 input NAND gate
+                    vm.circuitComponents[vm.idNum] = new TwoInputNAND(uniqueID, false, false, [], []);
+                    vm.idNum++;
+
+                });
+
+            });
+
+            $("#select2InputAND").click(function() {
+
+                Snap.load("src/components/circuitComponents/twoInputAND.vue", function(e) {
+                    newComponent = e.select("g");
+                    snap.append(newComponent);
+                    newComponent.drag();
+
+                    // Sets a unique id to the rendered component
+                    var newSVG = canvas.lastElementChild;
+                    var uniqueID = "component-" + vm.idNum;
+                    newSVG.setAttribute("id", uniqueID);
+
+                    // Create default 2 input AND gate
+                    vm.circuitComponents[vm.idNum] = new TwoInputAND(uniqueID, false, false, [], []);
+                    vm.idNum++;
+
+                });
+
+            });
+
+            $(document).on("dblclick", ".gate-output", function () {
+                vm.findOutputCoords($(this))
+            });
+            $(document).on("dblclick", ".gate-input", function () {
+                vm.findInputCoords($(this))
+            });
+            $(document).on("mouseup", ".drag-box", function () {
+                vm.redrawWires($(this)[0].parentNode)
+            });
             /**
              * Custom context menu script for the circuit designer.
              * STRUCTURE CODE PROVIDED BY SITEPOINT
              */
 
             (function() {
-                console.log(vm)
 
                 "use strict";
 
@@ -591,8 +944,6 @@
             },
 
             redrawWires (e) {
-
-                console.log(e)
 
                 let snap = Snap('#canvas')
 
