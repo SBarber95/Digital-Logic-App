603521c39ffabacf19c6c58a861024ac2bce5cf8
Savannah Barber <savannahlee117@gmail.com>
2017-08-21 09:02:05 -0400

    IEEE Dec conversion fix

--- src/components/subComponents/ieeeConversions.vue
+++ src/components/subComponents/ieeeConversions.vue
@@ -3,9 +3,9 @@
         <div id="convert-component">
             <form id="ieee-hex-converter">
                 <input type="text" class="ieee-hex-value" name="ieeeHexValue" placeholder="Enter an 8-digit IEEE Hex #" required>
-                <button id="converter_submit" class="btn btn-mdb" v-on:click="convertFromHex" name="submit">Convert</button>
+                <button id="converter_submit" class="btn btn-mdb" @click.prevent="convertFromHex" name="submit">Convert</button>
             </form>
-            <button id="toggle_direction" class="btn btn-mdb" v-on:click="toggleDirection">Toggle Direction</button>
+            <button id="toggle_direction" class="btn btn-mdb" @click="toggleDirection">Toggle Direction</button>
         </div>
         <div id="convert-quiz-component">
             <h2 id="second_header">Conversion Steps</h2>
@@ -19,9 +19,9 @@
         <div id="convert-component">
             <form id="ieee-dec-converter">
                 <input type="text" class="ieee-dec-value" name="ieeeDecValue" placeholder="Enter a Decimal Value" required>
-                <button id="converter_submit" class="btn btn-mdb" v-on:click="convertFromHex" name="submit">Convert</button>
+                <button id="converter_submit" class="btn btn-mdb" @click.prevent="convertFromDec" name="submit">Convert</button>
             </form>
-            <button id="toggle_direction" class="btn btn-mdb" v-on:click="toggleDirection">Toggle Direction</button>
+            <button id="toggle_direction" class="btn btn-mdb" @click="toggleDirection">Toggle Direction</button>
         </div>
         <div id="convert-quiz-component">
             <h2 id="second_header">Conversion Steps</h2>
@@ -74,9 +74,7 @@
             toggleDirection () {
                 this.toggled = !this.toggled
             },
-            convertFromHex (e) {
-
-                e.preventDefault();
+            convertFromHex () {
 
                 document.getElementById("output").setAttribute("style", "margin-top: 20px");
 
@@ -228,10 +226,8 @@
                 }
 
             },
-            convertFromDec (e) {
-
-                e.preventDefault();
-
+            convertFromDec () {
+                
                 document.getElementById("output").setAttribute("style", "margin-top: 20px");
 
                 var output = document.getElementById("output");
